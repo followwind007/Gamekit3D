fea3db673c31fc7bbe4d20022bf250d0648b33ef
albertzuo <zzhongyang@163.com>
2021-10-15 18:59:31 +0800

    urp tookit

---
+++ Packages/URPToolkit/Editor/MenuEntry.cs
@@ -0,0 +1,25 @@
+using UnityEditor;
+
+namespace GameApp.URPToolkit
+{
+    public static class MenuEntry
+    {
+        [MenuItem("Assets/Create/Shader/URP Lit Shader", false, 1001)]
+        public static void CreateLitShader()
+        {
+            
+        }
+
+        [MenuItem("Assets/Create/Shader/URP UnLit Shader", false, 1002)]
+        public static void CreateUnLitShader()
+        {
+            
+        }
+
+        [MenuItem("Assets/URPToolkit/Upgrade Selected Shaders", false, 2001)]
+        public static void UpdateSelectedShaders()
+        {
+            
+        }
+    }
+}
